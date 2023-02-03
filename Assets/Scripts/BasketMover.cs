6d0bc4a294144e116c25a7c4799c3ae07c0ba663
Erik Högberg <erik@e.email>
2023-02-03 23:40:51 +0100

    basket wip

---
+++ Assets/Scripts/BasketMover.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasketMover : MonoBehaviour
+{
+    public Transform Plane;
+    public Rigidbody Rb;
+
+    private void FixedUpdate() {
+    // TODO: project mouse pos on plane, move basket rb there
+        
+    }
+}
