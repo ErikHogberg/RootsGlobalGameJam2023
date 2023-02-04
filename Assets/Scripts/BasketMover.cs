6c6e3da699d3518c8595e90378c660f1f65467e9
Erik Högberg <erik@e.email>
2023-02-04 12:24:01 +0100

    spatuala

--- Assets/Scripts/BasketMover.cs
+++ Assets/Scripts/BasketMover.cs
@@ -1,14 +1,66 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class BasketMover : MonoBehaviour
 {
-    public Transform Plane;
+    public Transform GaemPlane;
+    public Camera Cam;
     public Rigidbody Rb;
 
-    private void FixedUpdate() {
-    // TODO: project mouse pos on plane, move basket rb there
-        
+    [Space]
+    public float ForceMul = 1f;
+
+    private void Start()
+    {
+        if (!Rb) Rb = GetComponent<Rigidbody>();
+        if (!Rb)
+        {
+            enabled = false;
+            Debug.LogWarning($"basket {gameObject.name} is missing its rb");
+            return;
+        }
+
+        if (!GaemPlane) GaemPlane = transform.parent;
+    }
+
+    private void FixedUpdate()
+    {
+        // if(!Pointer.current.IsPressed()) return;
+
+        // TODO: project mouse pos on plane, move basket rb there
+
+        Plane plane = new Plane(
+            GaemPlane.position + GaemPlane.up,
+            GaemPlane.position + GaemPlane.right,
+            GaemPlane.position
+        );
+        Ray ray = Cam.ScreenPointToRay(
+                        // Pointer.current.position.ReadValue()
+                        Mouse.current.position.ReadValue()
+                    );
+        if (plane.Raycast(
+            ray, out float enter))
+        {
+
+            Vector3 basketPos = ray.GetPoint(enter);
+            // Plane.TransformPoint(
+            // Vector3.ProjectOnPlane(
+            // Plane.InverseTransformPoint(
+
+            // ),
+            // Vector3.forward
+            // )
+            // )
+            ;
+            Rb.MovePosition(basketPos);
+            Debug.Log($"New basket pos {basketPos.ToString("0.00")}");
+        }
+
+        // transform.position = basketPos;
+        // Vector3 force = (Rb.position - basketPos) * ForceMul;
+        // Debug.Log($"Adding basket force {force.ToString("0.00")}");
+        // Rb.AddForce(force, ForceMode.Force);
     }
 }
