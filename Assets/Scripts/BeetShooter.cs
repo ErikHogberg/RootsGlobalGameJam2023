da0f536522a76e60bcc80ec668d4b68e8fb6316c
Erik Högberg <erik@e.email>
2023-02-05 10:36:08 +0100

    beet shooter working

--- Assets/Scripts/BeetShooter.cs
+++ Assets/Scripts/BeetShooter.cs
@@ -10,14 +10,34 @@ public class BeetShooter : MonoBehaviour
     public Transform MinDir;
     public Transform MaxDir;
 
+    public float MinLaunchForce = 1;
+    public float MaxLaunchForce = 2;
+
     Random random;
 
     private void Start() {
         random = new((uint)System.DateTime.Now.Millisecond);
     }
 
+    private void OnDrawGizmosSelected() {
+        Gizmos.color = Color.grey;
+        Gizmos.DrawLine(MinDir.position, MinDir.position + MinDir.forward);
+        Gizmos.color = Color.red;
+        Gizmos.DrawLine(MaxDir.position, MaxDir.position + MaxDir.forward);
+    }
+
     public void Shoot(){
         float lerp = random.NextFloat(0, 1);
+
+
+        Vector3 pos = Vector3.Lerp(MinDir.position, MaxDir.position, lerp);
+        Quaternion rot = Quaternion.Lerp(MinDir.rotation, MaxDir.rotation, lerp);
+
+        var newBeet = Instantiate(BeetPrefab, pos, rot);
+
+        lerp = random.NextFloat(0, 1);
+        newBeet.GetComponent<Rigidbody>().AddForce(rot * Vector3.forward * Mathf.Lerp(MinLaunchForce, MaxLaunchForce, lerp), ForceMode.VelocityChange);
+
     }
 
 }
